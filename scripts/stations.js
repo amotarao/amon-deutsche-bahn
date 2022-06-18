0b399a2bd3390798313788fdb115b1da348cb644
Amon Sawamura <amotarao@gmail.com>
2022-06-18 10:53:13 +0900

    fix station script

--- scripts/stations.js
+++ scripts/stations.js
@@ -1,6 +1,7 @@
-import { applicationDefault, initializeApp } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
-import fetch from 'node-fetch';
+import * as geofire from "geofire-common";
+import { applicationDefault, initializeApp } from "firebase-admin/app";
+import { getFirestore, FieldValue } from "firebase-admin/firestore";
+import fetch from "node-fetch";
 
 initializeApp({
   credential: applicationDefault(),
@@ -9,17 +10,38 @@ initializeApp({
 const db = getFirestore();
 
 const single = async (offset) => {
-  const res = await fetch(`https://apis.deutschebahn.com/db-api-marketplace/apis/ris-stations/v1/stations?offset=${offset}`, {
-    headers: {
-      'DB-Client-Id': process.env.DB_CLIENT_ID,
-      'DB-Api-Key': process.env.DB_API_KEY,
-    },
-  });
+  const res = await fetch(
+    `https://apis.deutschebahn.com/db-api-marketplace/apis/ris-stations/v1/stations?offset=${offset}`,
+    {
+      headers: {
+        "DB-Client-Id": process.env.DB_CLIENT_ID,
+        "DB-Api-Key": process.env.DB_API_KEY,
+      },
+    }
+  );
   const json = await res.json();
 
   const bulkWriter = db.bulkWriter();
   json.stations.forEach((station) => {
-    bulkWriter.set(db.collection('stations').doc(station.stationID), station);
+    const lat = station.position ? station.position.latitude : 0;
+    const lng = station.position ? station.position.longitude : 0;
+    const geohash = geofire.geohashForLocation([lat, lng]);
+
+    const data = {
+      ...station,
+      names: FieldValue.delete(),
+      name: station.names.DE.name,
+      position: {
+        geohash,
+        lat,
+        latitude: FieldValue.delete(),
+        lng,
+        longitude: FieldValue.delete(),
+      },
+    };
+
+    const ref = db.collection("stations").doc(data.stationID);
+    bulkWriter.set(ref, data, { merge: true });
   });
   await bulkWriter.close();
 };
