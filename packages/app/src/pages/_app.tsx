b4af12610236c9ba3d1438e52b4d4bf0f90fb10d
Amon Sawamura <amotarao@gmail.com>
2022-06-20 11:24:05 +0900

    add nav

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import type { AppProps } from 'next/app';
+import Link from 'next/link';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <nav className="flex gap-2 bg-slate-500 p-4 text-white">
+        <Link href="/stations">
+          <a className="text-sm">Stations</a>
+        </Link>
+        <Link href="/timetable/stations/Frankfurt Hbf">
+          <a className="text-sm">Timetable</a>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
