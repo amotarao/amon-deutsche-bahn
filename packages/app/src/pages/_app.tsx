3befcbbfaffe2ee93c4ad11371d722e21675f8c6
Amon Sawamura <amotarao@gmail.com>
2022-06-25 18:53:47 +0900

    hide navbar

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -5,14 +5,16 @@ import '../styles/globals.css';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <nav className="flex gap-2 bg-slate-500 p-4 text-white">
-        <Link href="/stations">
-          <a className="text-sm">Stations</a>
-        </Link>
-        <Link href="/timetable/stations/Frankfurt Hbf">
-          <a className="text-sm">Timetable</a>
-        </Link>
-      </nav>
+      {false && (
+        <nav className="flex gap-2 bg-slate-500 p-4 text-white">
+          <Link href="/stations">
+            <a className="text-sm">Stations</a>
+          </Link>
+          <Link href="/timetable/stations/Frankfurt Hbf">
+            <a className="text-sm">Timetable</a>
+          </Link>
+        </nav>
+      )}
       <Component {...pageProps} />
     </>
   );
