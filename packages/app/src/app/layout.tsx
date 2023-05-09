ab7cad162d5589b67d04c8de197659060c1273ab
Amon Sawamura <amotarao@gmail.com>
2023-05-09 14:06:55 +0200

    App Router: RootLayout

---
+++ packages/app/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import '../styles/globals.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {false && (
+          <nav className="flex gap-2 bg-slate-500 p-4 text-white">
+            <Link className="text-sm" href="/stations">
+              Stations
+            </Link>
+            <Link className="text-sm" href="/timetable/stations/Frankfurt Hbf">
+              Timetable
+            </Link>
+          </nav>
+        )}
+        {children}
+      </body>
+    </html>
+  );
+}
