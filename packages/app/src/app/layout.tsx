a718e037b4c0d916a434404c3b96f66fea10816b
Amon Sawamura <amotarao@gmail.com>
2025-05-12 02:00:50 +0900

    style.css 移動

--- packages/app/src/app/layout.tsx
+++ packages/app/src/app/layout.tsx
@@ -1,10 +1,10 @@
-import "../styles/globals.css";
+import "./style.css";
 
-export default function RootLayout({
-  children,
-}: {
+type Props = {
   children: React.ReactNode;
-}) {
+};
+
+export default function RootLayout({ children }: Props) {
   return (
     <html lang="de" className="dark:bg-slate-900 dark:text-gray-100">
       <body className="pb-40">{children}</body>
